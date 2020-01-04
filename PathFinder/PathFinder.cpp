54b344d6cdab5b6b5239c9e595867ea669e8d5bb
jyyblue <jyyblue1987@outlook.com>
2020-01-04 03:16:54 +0200

    [add] parse value.

--- PathFinder/PathFinder.cpp
+++ PathFinder/PathFinder.cpp
@@ -8,7 +8,8 @@
 #include <iostream>
 using namespace std;
 
-#define COLUMN_COUNT	4
+#define MAX_ROW_COUNT	200
+#define COLUMN_COUNT	16
 #define DIGIT_COUNT		3
 
 void generatePermList(int *perm_list, int level, int &count)
@@ -31,6 +32,22 @@ void generatePermList(int *perm_list, int level, int &count)
 	}
 }
 
+int* parseValue(char *val)
+{
+	if( val == NULL || strlen(val) < 1 )
+		return NULL;
+
+	int count = strlen(val);
+	int* x = (int *)calloc(count, sizeof(int));
+
+	int i = 0; 
+
+	for(i = 0; i < count; i++)
+		x[i] = val[i] - 48;
+
+	return x;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int i = 0, j = 0;
@@ -47,14 +64,40 @@ int _tmain(int argc, _TCHAR* argv[])
 	int count = 0;
 	generatePermList(perm_list, 0, count);
 
-	for(i = 0; i < PERM_TOTAL_COUNT; i += DIGIT_COUNT )
+	//for(i = 0; i < PERM_TOTAL_COUNT; i += DIGIT_COUNT )
+	//{
+	//	for(j = 0; j < DIGIT_COUNT; j++)
+	//		cout << perm_list[i + j];
+	//	cout << endl;
+	//}
+
+	int row_count = 12;
+	char x[][COLUMN_COUNT + 1] = {
+		"1213232312321122",
+		"3232111213213121",
+		"2111323231231213",
+		"2323112323121213",
+		"1213233321231231",
+		"2111323132231222",
+		"1123133212311123",
+		"3233123132313211",
+		"3112312312323121",
+		"1232312312321113",
+		"2132311232312123",
+		"3121213232112312"
+	}; 
+	 
+	int *xx[MAX_ROW_COUNT];
+	memset(xx, 0, MAX_ROW_COUNT * sizeof(int*));
+
+	for(i = 0; i < row_count; i++)
 	{
-		for(j = 0; j < DIGIT_COUNT; j++)
-			cout << perm_list[i + j];
-		cout << endl;
+		xx[i] = parseValue(x[i]);
 	}
-	
+
 	free(perm_list);
+	for(i = 0; i < row_count; i++)
+		free(xx[i]);
 
 	return 0;
 }
