5e32de5e426ce66593b9844b3cf632b88ba8d929
jyyblue <jyyblue1987@outlook.com>
2020-01-04 02:50:59 +0200

    [create] create project.

---
+++ PathFinder/PathFinder.cpp
@@ -0,0 +1,60 @@
+// PathFinder.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+
+#include <iostream>
+using namespace std;
+
+#define COLUMN_COUNT	4
+#define DIGIT_COUNT		3
+
+void generatePermList(int *perm_list, int level, int &count)
+{
+	if( level == DIGIT_COUNT )
+	{
+		count++;
+		return;
+	}
+
+	int i = 0;
+	for(i = 1; i <= COLUMN_COUNT; i++)
+	{		
+		generatePermList(perm_list, level + 1, count);
+		perm_list[DIGIT_COUNT * count + level] = i;
+	}
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int i = 0, j = 0;
+	int PERM_TOTAL_COUNT  = DIGIT_COUNT;
+
+	for(i = 0; i < DIGIT_COUNT; i++)
+		PERM_TOTAL_COUNT *= COLUMN_COUNT;
+
+	int *perm_list = (int*)calloc(PERM_TOTAL_COUNT, sizeof(int));
+	if( perm_list == NULL )
+		return -1;
+
+	// generate perm list
+	int count = 0;
+	generatePermList(perm_list, 0, count);
+
+	for(i = 0; i < PERM_TOTAL_COUNT; i += DIGIT_COUNT )
+	{
+		for(j = 0; j < DIGIT_COUNT; j++)
+			cout << perm_list[i + j];
+		cout << endl;
+	}
+	
+
+
+
+	free(perm_list);
+
+	return 0;
+}
+
